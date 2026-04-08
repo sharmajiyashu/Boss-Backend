c33b8f3ae3836c6b7265a0de9b8dd0d11b167bc3
jangidkapilyashu <jangidkapilyashu@gmail.com>
2026-04-09 00:02:51 +0530

    update changes in this

--- src/constants/appWhitelistRoutes.ts
+++ src/constants/appWhitelistRoutes.ts
@@ -1,10 +1,9 @@
 export const appWhitelistRoutes = [
     '/auth/send-otp',
     '/auth/verify-otp',
-    '/home',
+    '/auth/register',
+    '/auth/login',
     '/products',
     '/subcategories',
     '/categories',
-    '/temples',
-    '/tours',
 ]
